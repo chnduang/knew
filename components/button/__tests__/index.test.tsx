6d38d613f5c739595fc5b059aa5e362c4ab80f88
zhouqd <zhouqd1997@163.com>
2021-09-26 16:10:14 +0800

    [ADD] 更改脚本

--- components/button/__tests__/index.test.tsx
+++ components/button/__tests__/index.test.tsx
@@ -5,8 +5,6 @@ import mountTest from '../../../tests/shared/mountTest';
 
 describe('Button', () => {
   mountTest(Button);
-  mountTest(() => <Button size="large" />);
-  mountTest(() => <Button size="small" />);
 
   it('renders correctly', () => {
     expect(<Button>Follow</Button>).toMatchRenderedSnapshot();
